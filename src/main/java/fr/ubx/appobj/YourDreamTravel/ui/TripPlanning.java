9bdb3d0544564e7d4cadf54f7e73eabd55f248d8
gduboureau <guillaume.duboureau@etu.u-bordeaux.fr>
2022-12-14 12:47:54 +0100

    rendu final

--- src/main/java/fr/ubx/appobj/YourDreamTravel/ui/TripPlanning.java
+++ src/main/java/fr/ubx/appobj/YourDreamTravel/ui/TripPlanning.java
@@ -1,11 +1,9 @@
 package main.java.fr.ubx.appobj.YourDreamTravel.ui;
 
 import java.io.BufferedReader;
-// import java.io.Bufferedtext;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.DateFormat;
-// import java.io.InputStreamtext;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
